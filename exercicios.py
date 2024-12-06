1ffc74415574145e6235b4d3adce75172b30ea18
Lucas <lucas-mvs@hotmail.com>
2024-12-05 21:42:02 -0300

    adicionando exercicios

--- exercicios.py
+++ exercicios.py
@@ -1,4 +1,12 @@
+# 1
 num_in = int(input("Inserir um numero inteiro: "))
 num_in_2 = int(input("Inserir um denominador: "))
 resultado = num_in // num_in_2
-print(resultado)
+print(resultado) 
+
+# 2
+import math 
+raio_circulo = float(input("Insira o raio: "))
+area_circ = math.pi *raio_circulo** 2
+# area_format = "{:.2f}".format(area_circ)
+print(f"{area_circ:.2f}")
