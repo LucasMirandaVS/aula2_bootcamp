2d8430e32b013e4ed990947d3c206893c1ec1b27
Lucas <lucas-mvs@hotmail.com>
2024-12-09 18:24:35 -0300

    adicionando o terceiro exercicio

--- exercicios.py
+++ exercicios.py
@@ -10,3 +10,8 @@ raio_circulo = float(input("Insira o raio: "))
 area_circ = math.pi *raio_circulo** 2
 # area_format = "{:.2f}".format(area_circ)
 print(f"{area_circ:.2f}")
+
+# 3 
+data_do_user = input("Insira uma data no formato dd/mm/aaaa: ")
+data_separada = data_do_user.split("/")
+print(data_separada)
